d8cd0f5251b620a1fa630781b3f5d98414ea58a4
Barzoius <mecca.rares@gmail.com>
2026-01-23 14:55:08 +0200

    leaderBoard

--- Assets/PlayFab/Scripts/PanelController.cs
+++ Assets/PlayFab/Scripts/PanelController.cs
@@ -1,3 +1,4 @@
+
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,18 +10,53 @@ public class PanelController : MonoBehaviour
 
     public TMP_Text displayName;
 
+    public GameObject leaderboard;
+    public GameObject mainManu;
+    public GameObject customization;
+
+
+    public TMP_Text p1;
+    public TMP_Text p2;
+    public TMP_Text p3;
+    public TMP_Text p4;
+    public TMP_Text p5;
+
+
 
     void Start()
     {
-        //myButton.onClick.AddListener(() => PlayFabController.Instance.StartCloudUpdatePlayerStats());
-        //myButton2.onClick.AddListener(() => PlayFabController.Instance.GetLeaderboard());
+        myButton.onClick.AddListener(() => PlayFabController.Instance.GetPlayerData());
+        myButton2.onClick.AddListener(() => PlayFabController.Instance.GetLeaderboard());
 
         PlayFabController.Instance.OnDisplayNameLoaded += SetDisplayName;
+        PlayFabController.Instance.OnLeaderboardUpdated += SetLeaderboard;
 
-        // Optional: handle case where name already loaded
         if (!string.IsNullOrEmpty(PlayFabController.Instance.displayName))
         {
             SetDisplayName(PlayFabController.Instance.displayName);
+        }
+
+      
+    }
+
+    public void SetLeaderboard()
+    {
+     
+
+        TMP_Text[] textFields = new TMP_Text[] { p1, p2, p3, p4, p5 };
+
+        for (int i = 0; i < PlayFabController.Instance.players.Count && i < textFields.Length; i++)
+        {
+         
+            if (PlayFabController.Instance.players[i] != null)
+            {
+              
+                textFields[i].text = (i + 1) + ". " + PlayFabController.Instance.players[i].DisplayName + " [ " + PlayFabController.Instance.players[i].StatValue + " ]";
+            }
+            else
+            {
+                Debug.Log("no PLAYERRS");
+            }
         }
     }
 
@@ -35,4 +71,25 @@ public class PanelController : MonoBehaviour
         if (PlayFabController.Instance != null)
             PlayFabController.Instance.OnDisplayNameLoaded -= SetDisplayName;
     }
+
+
+    public void LoadLeaderBoard()
+    {
+        leaderboard.SetActive(true);
+        mainManu.SetActive(false);
+    }
+
+    public void LoadCustomization()
+    {
+        customization.SetActive(true);
+        mainManu.SetActive(false);
+    }
+
+    public void LoadMainManu()
+    {
+        mainManu.SetActive(true);
+        customization.SetActive(false);
+        leaderboard.SetActive(false);
+    }
+
 }
